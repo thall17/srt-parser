3dabac419f154b59b547f5c87d613cc7fa6d2e98
Tim Hall <thall74@gatech.edu>
2017-11-14 21:39:14 -0800

    Try to fix newline issue

--- parse_srt.py
+++ parse_srt.py
@@ -5,29 +5,7 @@
 import sys
 import pysrt
 import os
-
-# Helper functions
-
-
-def clean_sub(s):
-    s = remove_carats(s)
-    s = add_spacing(s)
-
-    return s
-
-def add_spacing(s):
-    new_string = s
-    new_string += " "
-    return new_string
-
-
-def remove_carats(s):
-    if s[0:2] == ">>":
-        new_string = s[2:-1]
-    else:
-        new_string = s
-    return new_string
-
+import csv
 
 # args must be 2 or 3 params long [script_name, src_dir, dest_dir]
 if (len(sys.argv) < 2) or (len(sys.argv) > 3):
@@ -37,7 +15,27 @@ if (len(sys.argv) < 2) or (len(sys.argv) > 3):
     print "[dest_dir] is optional, and will default to src_dir if not provided"
     exit()
 
+# Helper functions
+def clean_sub(s):
+    s = s.rstrip()
+    s = remove_carats(s)
+    s = add_spacing(s)
+    return s
+
+def add_spacing(s):
+    new_string = s
+    new_string += " "
+    return new_string
+
+def remove_carats(s):
+    if s[0:2] == ">>":
+        new_string = s[2:-1]
+    else:
+        new_string = s
+    return new_string
+
 srt_file = sys.argv[1]
+file_name = srt_file[0:-4]
 subs = pysrt.open(srt_file)
 
 transcript = ""
@@ -48,6 +46,19 @@ for sub in subs:
     print "text = " + text
     transcript += text
 
+# transcript = transcript.rstrip()
+transcript = transcript.replace(",", ";")
+# transcript = "|" + transcript + "|"
+
+
+
+with open('Test.csv', 'wb') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    filewriter.writerow(['Name', 'Profession', 'Answer'])
+    filewriter.writerow([file_name, file_name, transcript])
+
+
 print "srt_file = " + str(srt_file)
 print ""
 print "transcript = " + transcript
@@ -55,6 +66,8 @@ print ""
 print "subs = " + str(subs)
 print "Current working dir : %s" % os.getcwd()
 print "os.path.dirname... is %s" % os.path.dirname(os.path.realpath(sys.argv[0]))
+print ""
+print "subs[0].text = " + subs[0].text.rstrip()
 print 'Script Finished'
 
 
