2370e6187ad156a363a7e5e2ac382834395c689c
Tim Hall <timhall3000@gmail.com>
2017-11-14 16:11:33 -0800

    Add param check

--- parse_srt.py
+++ parse_srt.py
@@ -5,8 +5,17 @@
 
 import sys
 import pysrt
+import os
 
-if len(sys.argv) != 2:
-    print "Syntax:"
-    print "    python parse_srt.py <directory_containing_srt_files>"
+if not 1 < len(sys.argv) < 4:
+    print "Wrong # of parameters."
+    print "Syntax:    'python parse_srt.py <src_dir> [dest_dir]'"
+    print "<src_dir> is required and should contain .srt files, or contain folders which each contain .srt files."
+    print "[dest_dir] is optional, and will default to src_dir if not provided"
     exit()
+
+# subs = pysrt.open('some/file.srt')
+
+print "Current working dir : %s" % os.getcwd()
+print "os.path.dirname... is %s" % os.path.dirname(os.path.realpath(sys.argv[0]))
+print 'Script Finished'
